255174536b54c1040dbf5a11622bdf59eee8857b
himanshu <himanshumehta1114@gmail.com>
2017-11-17 14:58:16 +0530

    Created server|request headers

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/',(req,res) => {
+  console.log(req.headers);
+});
+
+app.listen(port,() => {
+  console.log(`Server is up on port ${port}`);
+});
