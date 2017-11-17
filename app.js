498bce79555c88a97155c52ceb412c615a2a8581
himanshu <himanshumehta1114@gmail.com>
2017-11-17 15:20:34 +0530

    header parsing complete

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/',(req,res) => {
-  console.log(req.headers);
-  res.send(req.headers);
+  // console.log(req.headers);
+  // res.send(req.headers);
+  var ip = req.headers['x-forwarded-for'];
+  var software = req.headers['user-agent'].split('(')[1].split(')')[0];
+  var language = req.headers['accept-language'].split(',')[0];
+
+  var jsonData = {
+    'ipaddress' : ip,
+    language,
+    software
+  };
+  res.send(jsonData);
 });
 
 app.listen(port,() => {
